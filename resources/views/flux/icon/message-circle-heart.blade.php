f5aaf0bd4460f90b741a3a7783b7b85da9dae064
Janus Helkjær <janus.helkjaer@gmail.com>
2024-11-26 23:17:19 +0100

    flux ui + daisyui

---
+++ resources/views/flux/icon/message-circle-heart.blade.php
@@ -0,0 +1,42 @@
+{{-- Credit: Lucide (https://lucide.dev) --}}
+
+@props([
+    'variant' => 'outline',
+])
+
+@php
+if ($variant === 'solid') {
+    throw new \Exception('The "solid" variant is not supported in Lucide.');
+}
+
+$classes = Flux::classes('shrink-0')
+    ->add(match($variant) {
+        'outline' => '[:where(&)]:size-6',
+        'solid' => '[:where(&)]:size-6',
+        'mini' => '[:where(&)]:size-5',
+        'micro' => '[:where(&)]:size-4',
+    });
+
+$strokeWidth = match ($variant) {
+    'outline' => 2,
+    'mini' => 2.25,
+    'micro' => 2.5,
+};
+@endphp
+
+<svg
+    {{ $attributes->class($classes) }}
+    data-flux-icon
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="{{ $strokeWidth }}"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    aria-hidden="true"
+    data-slot="icon"
+>
+  <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
+  <path d="M15.8 9.2a2.5 2.5 0 0 0-3.5 0l-.3.4-.35-.3a2.42 2.42 0 1 0-3.2 3.6l3.6 3.5 3.6-3.5c1.2-1.2 1.1-2.7.2-3.7" />
+</svg>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,45 +1,142 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @livewire('navigation-menu')
+    <!-- Styles -->
+    @livewireStyles
+    @fluxStyles
+</head>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
+<body class="min-h-screen bg-white dark:bg-zinc-800">
+
+    <flux:header sticky class="bg-zinc-50 dark:bg-zinc-900 border-b border-zinc-200 dark:border-zinc-700">
+        <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+
+        <flux:brand href="#" logo="{{ asset('images/logo.png') }}" name="{{ config('app.name', 'Laravel') }}"
+            class="max-lg:hidden dark:hidden" />
+        <flux:brand href="#" logo="{{ asset('images/dark-mode-logo.png') }}"
+            name="{{ config('app.name', 'Laravel') }}" class="max-lg:!hidden hidden dark:flex" />
+
+        <flux:navbar class="-mb-px max-lg:hidden">
+            <flux:navbar.item icon="home" href="{{ route('dashboard') }}" current>Hjem</flux:navbar.item>
+            <flux:navbar.item icon="inbox" badge="12" href="#">Artikler</flux:navbar.item>
+            <flux:navbar.item icon="message-circle-heart" href="#">Forum</flux:navbar.item>
+            <flux:navbar.item icon="calendar" href="#">Shop</flux:navbar.item>
+
+            {{-- <flux:separator vertical variant="subtle" class="my-2" />
+
+            <flux:dropdown class="max-lg:hidden">
+                <flux:navbar.item icon-trailing="chevron-down">Favorites</flux:navbar.item>
+
+                <flux:navmenu>
+                    <flux:navmenu.item href="#">Marketing site</flux:navmenu.item>
+                    <flux:navmenu.item href="#">Android app</flux:navmenu.item>
+                    <flux:navmenu.item href="#">Brand guidelines</flux:navmenu.item>
+                </flux:navmenu>
+            </flux:dropdown> --}}
+        </flux:navbar>
+
+        <flux:spacer />
+
+        <flux:navbar class="mr-4">
+            <flux:navbar.item icon="magnifying-glass" href="#" label="Search" />
+            <flux:navbar.item class="max-lg:hidden" icon="cog-6-tooth" href="#" label="Settings" />
+            <flux:navbar.item class="max-lg:hidden" icon="information-circle" href="#" label="Help" />
+        </flux:navbar>
+
+        <flux:dropdown position="top" align="start">
+            @auth
+                <flux:profile avatar="{{ Auth::user()->gravatar }}" name="{{ Auth::user()->name }}" />
+            @else
+                <flux:profile avatar="" name="Login" />
+
+
+            @endauth
+
+            <flux:menu>
+                <flux:menu.item icon="user" href="{{ route('profile.show') }}">Profile
+                </flux:menu.item>
+
+
+
+                <flux:menu.separator />
+
+                <form method="POST" action="{{ route('logout') }}" x-data>
+                    @csrf
+
+                    <flux:menu.item icon="arrow-right-start-on-rectangle" type="submit" href="{{ route('logout') }}"
+                        @click.prevent="$root.submit();">Logout</flux:menu.item>
+                </form>
+
+
+
+
+            </flux:menu>
+        </flux:dropdown>
+    </flux:header>
+
+    <flux:sidebar stashable sticky
+        class="lg:hidden bg-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-700">
+        <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
+
+        <flux:brand href="#" logo="https://fluxui.dev/img/demo/logo.png" name="Acme Inc."
+            class="px-2 dark:hidden" />
+        <flux:brand href="#" logo="https://fluxui.dev/img/demo/dark-mode-logo.png" name="Acme Inc."
+            class="px-2 hidden dark:flex" />
+
+        <flux:navlist variant="outline">
+            <flux:navlist.item icon="home" href="#" current>Home</flux:navlist.item>
+            <flux:navlist.item icon="inbox" badge="12" href="#">Inbox</flux:navlist.item>
+            <flux:navlist.item icon="document-text" href="#">Documents</flux:navlist.item>
+            <flux:navlist.item icon="calendar" href="#">Calendar</flux:navlist.item>
+
+            <flux:navlist.group expandable heading="Favorites" class="max-lg:hidden">
+                <flux:navlist.item href="#">Marketing site</flux:navlist.item>
+                <flux:navlist.item href="#">Android app</flux:navlist.item>
+                <flux:navlist.item href="#">Brand guidelines</flux:navlist.item>
+            </flux:navlist.group>
+        </flux:navlist>
+
+        <flux:spacer />
+
+        <flux:navlist variant="outline">
+            <flux:navlist.item icon="cog-6-tooth" href="#">Settings</flux:navlist.item>
+            <flux:navlist.item icon="information-circle" href="#">Help</flux:navlist.item>
+        </flux:navlist>
+    </flux:sidebar>
+
+    <flux:main>
+        @if (isset($page_heading))
+            <flux:heading size="xl" level="1">{{ $page_heading }}</flux:heading>
+
+            @if (isset($page_subheading))
+                <flux:subheading size="lg">{{ $page_subheading }}</flux:subheading>
             @endif
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+            <flux:separator variant="subtle" class="mt-6" />
+        @endif
 
-        @stack('modals')
+        {{ $slot }}
+    </flux:main>
 
-        @livewireScripts
-    </body>
+
+    @stack('modals')
+
+    @livewireScripts
+    @fluxScripts
+</body>
+
 </html>
