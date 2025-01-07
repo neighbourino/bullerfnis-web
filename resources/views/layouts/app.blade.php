6071d1c7b5b5853bdb539a6999af1bb60e655749
Janus Helkjær <janus.helkjaer@gmail.com>
2025-01-07 22:51:42 +0100

    main header play

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,24 +24,41 @@
 
 <body class="min-h-screen bg-white dark:bg-zinc-800">
 
-    <flux:header sticky
-        class="bg-white dark:bg-zinc-900 border-zinc-100 dark:border-zinc-700 z-50 max-w-7xl mx-auto w-full border-b rounded-b  border-l border-r shadow-sm">
-        <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+    <div class="bg-zinc-50 py-3 h-12 w-full z-50 px-6">
+        <div class="px-6 max-w-7xl mx-auto grid grid-cols-4 grid-rows-1">
+            Top bar
+        </div>
+    </div>
+    <div class="bg-blue-50 py-3 h-24 w-full  z-50 px-6 max-w-7xl mx-auto grid grid-cols-4 grid-rows-1">
+        <div class="col-span-1 bg-yellow-500 flex items-center">
+            <a href="#" class=" max-h-full flex items-center">
+                <img src="{{ asset('images/logo.png') }}" alt="" class="block max-h-full h-16">
+                <span class="ml-3 text-4xl">bullerfnis</span>
+            </a>
+        </div>
+    </div>
 
-        <flux:brand href="#" logo="{{ asset('images/logo.png') }}" name="{{ config('app.name', 'Laravel') }}"
+
+    <div class="relative">
+        <flux:header sticky
+            class="bg-white dark:bg-zinc-900 border-zinc-100 dark:border-zinc-700 z-50 max-w-7xl mx-auto w-full border-b rounded-b  border-l border-r shadow-sm">
+            <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+
+
+            {{-- <flux:brand href="#" logo="{{ asset('images/logo.png') }}" name="{{ config('app.name', 'Laravel') }}"
             class="max-lg:hidden dark:hidden" href="/" />
         <flux:brand href="#" logo="{{ asset('images/dark-mode-logo.png') }}"
-            name="{{ config('app.name', 'Laravel') }}" class="max-lg:!hidden hidden dark:flex" href="/" />
+            name="{{ config('app.name', 'Laravel') }}" class="max-lg:!hidden hidden dark:flex" href="/" /> --}}
 
-        <flux:navbar class="-mb-px max-lg:hidden">
-            <flux:navbar.item href="{{ route('welcome') }}">Hjem</flux:navbar.item>
-            <flux:navbar.item href="{{ route('articles.index') }}">Artikler
-            </flux:navbar.item>
-            <flux:navbar.item href="{{ route('recipes.index') }}">Opskrifter</flux:navbar.item>
-            <flux:navbar.item href="#">Forum</flux:navbar.item>
-            <flux:navbar.item href="#">Shop</flux:navbar.item>
+            <flux:navbar class="-mb-px max-lg:hidden">
+                <flux:navbar.item href="{{ route('welcome') }}">Hjem</flux:navbar.item>
+                <flux:navbar.item href="{{ route('articles.index') }}">Artikler
+                </flux:navbar.item>
+                <flux:navbar.item href="{{ route('recipes.index') }}">Opskrifter</flux:navbar.item>
+                <flux:navbar.item href="#">Forum</flux:navbar.item>
+                <flux:navbar.item href="#">Shop</flux:navbar.item>
 
-            {{-- <flux:separator vertical variant="subtle" class="my-2" />
+                {{-- <flux:separator vertical variant="subtle" class="my-2" />
 
             <flux:dropdown class="max-lg:hidden">
                 <flux:navbar.item icon-trailing="chevron-down">Favorites</flux:navbar.item>
@@ -52,82 +69,83 @@
                     <flux:navmenu.item href="#">Brand guidelines</flux:navmenu.item>
                 </flux:navmenu>
             </flux:dropdown> --}}
-        </flux:navbar>
+            </flux:navbar>
 
-        <flux:spacer />
+            <flux:spacer />
 
-        <flux:navbar class="mr-4">
-            <flux:button x-on:click="$flux.dark = ! $flux.dark" icon="moon" variant="subtle"
-                aria-label="Toggle dark mode" />
-            <flux:navbar.item icon="magnifying-glass" href="#" label="Search" />
-            <flux:navbar.item class="max-lg:hidden" icon="cog-6-tooth" href="#" label="Settings" />
-            <flux:navbar.item class="max-lg:hidden" icon="information-circle" href="#" label="Help" />
-        </flux:navbar>
+            <flux:navbar class="mr-4">
+                <flux:button x-on:click="$flux.dark = ! $flux.dark" icon="moon" variant="subtle"
+                    aria-label="Toggle dark mode" />
+                <flux:navbar.item icon="magnifying-glass" href="#" label="Search" />
+                <flux:navbar.item class="max-lg:hidden" icon="cog-6-tooth" href="#" label="Settings" />
+                <flux:navbar.item class="max-lg:hidden" icon="information-circle" href="#" label="Help" />
+            </flux:navbar>
 
-        <flux:dropdown position="top" align="start">
-            @auth
-                <flux:profile avatar="{{ Auth::user()->gravatar }}" name="{{ Auth::user()->name }}" />
-            @else
-                <flux:profile avatar="" name="Login" />
-
-
-            @endauth
-
-            <flux:menu>
-                <flux:menu.item icon="user" href="{{ route('profile.show') }}">Profile
-                </flux:menu.item>
+            <flux:dropdown position="top" align="start">
+                @auth
+                    <flux:profile avatar="{{ Auth::user()->gravatar }}" name="{{ Auth::user()->name }}" />
+                @else
+                    <flux:profile avatar="" name="Login" />
 
 
+                @endauth
 
-                <flux:menu.separator />
-
-                <form method="POST" action="{{ route('logout') }}" x-data>
-                    @csrf
-
-                    <flux:menu.item icon="arrow-right-start-on-rectangle" type="submit" href="{{ route('logout') }}"
-                        @click.prevent="$root.submit();">Logout</flux:menu.item>
-                </form>
+                <flux:menu>
+                    <flux:menu.item icon="user" href="{{ route('profile.show') }}">Profile
+                    </flux:menu.item>
 
 
 
+                    <flux:menu.separator />
 
-            </flux:menu>
-        </flux:dropdown>
-    </flux:header>
+                    <form method="POST" action="{{ route('logout') }}" x-data>
+                        @csrf
 
-    <flux:sidebar stashable sticky
-        class="lg:hidden bg-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-700">
-        <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
+                        <flux:menu.item icon="arrow-right-start-on-rectangle" type="submit"
+                            href="{{ route('logout') }}" @click.prevent="$root.submit();">Logout</flux:menu.item>
+                    </form>
 
-        <flux:brand href="#" logo="https://fluxui.dev/img/demo/logo.png" name="Acme Inc."
-            class="px-2 dark:hidden" />
-        <flux:brand href="#" logo="https://fluxui.dev/img/demo/dark-mode-logo.png" name="Acme Inc."
-            class="px-2 hidden dark:flex" />
 
-        <flux:navlist variant="outline">
-            <flux:navlist.item icon="home" href="#" current>Home</flux:navlist.item>
-            <flux:navlist.item icon="inbox" badge="12" href="#">Inbox</flux:navlist.item>
-            <flux:navlist.item icon="document-text" href="#">Documents</flux:navlist.item>
-            <flux:navlist.item icon="calendar" href="#">Calendar</flux:navlist.item>
 
-            <flux:navlist.group expandable heading="Favorites" class="max-lg:hidden">
-                <flux:navlist.item href="#">Marketing site</flux:navlist.item>
-                <flux:navlist.item href="#">Android app</flux:navlist.item>
-                <flux:navlist.item href="#">Brand guidelines</flux:navlist.item>
-            </flux:navlist.group>
-        </flux:navlist>
 
-        <flux:spacer />
+                </flux:menu>
+            </flux:dropdown>
+        </flux:header>
 
-        <flux:navlist variant="outline">
-            <flux:navlist.item icon="cog-6-tooth" href="#">Settings</flux:navlist.item>
-            <flux:navlist.item icon="information-circle" href="#">Help</flux:navlist.item>
-        </flux:navlist>
-    </flux:sidebar>
+        <flux:sidebar stashable sticky
+            class="lg:hidden bg-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-700">
+            <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
-    <flux:main>
-        {{ $slot }}
-    </flux:main>
+            <flux:brand href="#" logo="https://fluxui.dev/img/demo/logo.png" name="Acme Inc."
+                class="px-2 dark:hidden" />
+            <flux:brand href="#" logo="https://fluxui.dev/img/demo/dark-mode-logo.png" name="Acme Inc."
+                class="px-2 hidden dark:flex" />
+
+            <flux:navlist variant="outline">
+                <flux:navlist.item icon="home" href="#" current>Home</flux:navlist.item>
+                <flux:navlist.item icon="inbox" badge="12" href="#">Inbox</flux:navlist.item>
+                <flux:navlist.item icon="document-text" href="#">Documents</flux:navlist.item>
+                <flux:navlist.item icon="calendar" href="#">Calendar</flux:navlist.item>
+
+                <flux:navlist.group expandable heading="Favorites" class="max-lg:hidden">
+                    <flux:navlist.item href="#">Marketing site</flux:navlist.item>
+                    <flux:navlist.item href="#">Android app</flux:navlist.item>
+                    <flux:navlist.item href="#">Brand guidelines</flux:navlist.item>
+                </flux:navlist.group>
+            </flux:navlist>
+
+            <flux:spacer />
+
+            <flux:navlist variant="outline">
+                <flux:navlist.item icon="cog-6-tooth" href="#">Settings</flux:navlist.item>
+                <flux:navlist.item icon="information-circle" href="#">Help</flux:navlist.item>
+            </flux:navlist>
+        </flux:sidebar>
+
+        <flux:main>
+            {{ $slot }}
+        </flux:main>
+    </div>
 
     {{-- <footer class="bg-gray-50 -mx-4 -mb-4 lg:-mx-8 lg:-mb-8">
         <div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
